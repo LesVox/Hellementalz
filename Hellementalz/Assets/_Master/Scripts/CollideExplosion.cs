9e3890e608760d1c158c4a24da948b517b218348
hurracharlie <superkalle123@hotmail.com>
2016-05-07 16:14:30 +0200

    Fireballz and explosions

---
+++ Hellementalz/Assets/_Master/Scripts/CollideExplosion.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideExplosion : MonoBehaviour {
+
+	public GameObject explosionPrefab;
+	public LayerMask hitLayer;
+	public float explosionRadius = 5f;
+	public float explosionPower = 10f;
+
+	private GameObject explosionObject;
+
+	void Start(){
+		explosionObject = (GameObject)Instantiate(explosionPrefab,transform.position, Quaternion.identity);
+	}
+
+	void AddExplosionForce(float radius, float power){
+		Vector3 explosionPos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere (explosionPos, radius);
+
+		foreach (Collider hit in colliders) {
+			Rigidbody rb = hit.GetComponent<Rigidbody> ();
+
+			if (rb != null) {
+				rb.AddExplosionForce (power, explosionPos, radius, 3f);
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col){
+
+		explosionObject.SetActive(false);
+		explosionObject.transform.position = transform.position;
+		explosionObject.SetActive(true);
+		gameObject.SetActive(false);
+		AddExplosionForce (explosionRadius, explosionPower);
+
+	}
+
+}
