7e5f27851ba159a73a585b968a9002c3c5870e12
hurracharlie <superkalle123@hotmail.com>
2016-07-07 10:59:11 +0200

    HandParticles

---
+++ Hellementalz/Assets/_Master/Scripts/Spells/Spells/HandTrackingParticle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandTrackingParticle : MonoBehaviour {
+
+	public ControllerTracker controller;
+	private EffectSettings effect;
+
+	// Use this for initialization
+	void Start () {
+		effect = GetComponent<EffectSettings> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller != null && effect != null) {
+			if (controller.IsTracking) {
+				effect.IsVisible = true;
+			} else
+				effect.IsVisible = false;
+		}			
+	}
+}
